525b074df95850e7736bfd9d0b1c550e0194e704
lona9a <adrianbeckdev@gmail.com>
2021-08-22 10:56:22 +0200

    Added dai

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -3,8 +3,12 @@
 export const FUJI_TOKEN_LIST =
   'https://raw.githubusercontent.com/Polarfox-DEX/polarfox-token-lists/master/43113/fuji-token-list.json'
 
+export const AVALANCHE_TOKEN_LIST =
+  'https://raw.githubusercontent.com/Polarfox-DEX/polarfox-token-lists/master/43114/avalanche-token-list.json'
+
 export const DEFAULT_TOKEN_LIST_URL = FUJI_TOKEN_LIST // TODO: Update
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [FUJI_TOKEN_LIST, DEFAULT_TOKEN_LIST_URL]
 
+  
 // TODO: Have the token lists based on chain ID. Given how the code is written, this might be difficult
