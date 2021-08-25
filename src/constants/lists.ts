3189f92d5eea67d37a1a5ae40170a14a2160f07b
lona9a <adrianbeckdev@gmail.com>
2021-08-25 08:18:59 +0200

    Fixed some merge issues

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -3,8 +3,8 @@
 export const AVALANCHE_TOKEN_LIST = 'https://raw.githubusercontent.com/Polarfox-DEX/polarfox-token-lists/avalanche-token-list/43114/avalanche-token-list.json'
 
 export const DEFAULT_TOKEN_LIST_URL = AVALANCHE_TOKEN_LIST // TODO: Update
-=======
-export const DEFAULT_TOKEN_LIST_URL = FUJI_TOKEN_LIST // TODO: Update
+
+export const DEFAULT_TOKEN_LIST_URL = AVALANCHE_TOKEN_LIST // TODO: Update
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [AVALANCHE_TOKEN_LIST, DEFAULT_TOKEN_LIST_URL]
 
