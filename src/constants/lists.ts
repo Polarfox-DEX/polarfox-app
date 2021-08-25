4a70afec74501e7b732627ee1acdb4a124c243c2
lona9a <adrianbeckdev@gmail.com>
2021-08-25 08:17:23 +0200

    Fixed some merge conflicts

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,15 +1,10 @@
 // the Polarfox Default token list lives here
-// TODO: Add Avalanche token lists
-//export const FUJI_TOKEN_LIST =
-  //'https://raw.githubusercontent.com/Polarfox-DEX/polarfox-token-lists/master/43113/fuji-token-list.json'
-
-<<<<<<< HEAD
+// TODO: Add Avalanche token li
 export const AVALANCHE_TOKEN_LIST = 'https://raw.githubusercontent.com/Polarfox-DEX/polarfox-token-lists/avalanche-token-list/43114/avalanche-token-list.json'
 
 export const DEFAULT_TOKEN_LIST_URL = AVALANCHE_TOKEN_LIST // TODO: Update
 =======
 export const DEFAULT_TOKEN_LIST_URL = FUJI_TOKEN_LIST // TODO: Update
->>>>>>> 49a929954cac925af4a7144ade8bbd3f0d667099
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [AVALANCHE_TOKEN_LIST, DEFAULT_TOKEN_LIST_URL]
 
