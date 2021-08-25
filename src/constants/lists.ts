1a259f8386bab839929383ba29f3c99685c97d89
lona9a <adrianbeckdev@gmail.com>
2021-08-25 08:21:52 +0200

    Working base build

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -4,8 +4,6 @@ export const AVALANCHE_TOKEN_LIST = 'https://raw.githubusercontent.com/Polarfox-
 
 export const DEFAULT_TOKEN_LIST_URL = AVALANCHE_TOKEN_LIST // TODO: Update
 
-export const DEFAULT_TOKEN_LIST_URL = AVALANCHE_TOKEN_LIST // TODO: Update
-
 export const DEFAULT_LIST_OF_LISTS: string[] = [AVALANCHE_TOKEN_LIST, DEFAULT_TOKEN_LIST_URL]
 
 // TODO: Have the token lists based on chain ID. Given how the code is written, this might be difficult
